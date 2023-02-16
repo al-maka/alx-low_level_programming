fa2f3bf0a09af376974bab501cd5ef8425f07db5
root <al-maka>
2023-02-16 13:02:26 -0800

    Hello, puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio>
 /**
- * main printf function
+ * main - printf function
  * return:0
  */
 int main(void){
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n")
 return (0);
 }
