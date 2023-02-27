cf9cdd98a97c8a4f2f486b979d364b106a9336b3
root <al-maka>
2023-02-27 12:39:56 -0800

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * void reset_to_98 - function that takes a pointer to an int
+ * reset_to_98 - function that takes a pointer to an int
  * as parameter and updates the value it points to to 98.
  * @n: integer
  * Return: empty
